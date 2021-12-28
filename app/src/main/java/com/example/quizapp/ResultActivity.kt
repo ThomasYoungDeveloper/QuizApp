8c5c0485462643a525c2901721683164621945a3
Thomas Young <Thomas.young228@gmail.com>
2021-12-27 23:29:51 -0500

    - finished app functionality

---
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -0,0 +1,29 @@
+package com.example.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val tvName: TextView = findViewById(R.id.tvName)
+        val tvScore: TextView = findViewById(R.id.tvScore)
+        val btnFinish: Button = findViewById(R.id.btnFinish)
+
+        tvName.text = intent.getStringExtra(Constants.USER_NAME)
+
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val totalScore = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        tvScore.text = "Your score is $totalScore out of $totalQuestions"
+
+        btnFinish.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
